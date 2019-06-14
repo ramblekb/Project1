d17008f5be254f690fa62ec49c19aa0c7aab91cf
Joe P <joe.peterson2729@gmail.com>
2019-06-13 19:13:49 -0500

    Added click function that stores fav artist list into list and also made changes to html ids to pull id to javascript

---
+++ assets/app.js
@@ -0,0 +1,87 @@
+
+
+$(document).ready(function () {
+    // Variable that stores the favorite artist:
+    var favoritelist = [];
+
+
+    // Function called when the form is submitted.
+    // Function adds favorite artist to the global array.
+    function addfavartist() {
+
+        var task = document.getElementById("nameInput");
+        var output = document.getElementById("userSearch");
+        var message = "";
+
+        if (task.value) {
+            favoritelist.push(task.value);
+            message = "";
+            for (var i = 0, count = favoritelist.length; i < count; i++) {
+                message += '<li>' + favoritelist[i] + '</li>';
+            }
+
+            output.innerHTML = message;
+        } // End of task.value IF.
+
+        // Return false to prevent submission:
+        return false;
+
+    }
+
+
+    function removeTask() {
+
+        alert("Button Clicked!");
+
+    }
+
+    $("#task").on("click", function (event) {
+        event.preventDefault();
+        addfavartist();
+    })
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById("nameInput").onclick  = function() {
+
+    //var favoritelist = document.createElement("Li");
+    //var text = document.getElementById("userSearch").value; 
+    //var textnode=document.createTextNode(text);
+    //node.appendChild(textnode);
+    //document.getElementById("userSearch").appendChild(node);
+
+
+
+
+//var userSearch = "";
+//$(document).ready(function() {
+//$("#buttonA").click(function() {
+    //userSearch = document.getElementById("userSearch").value;
+    //console.log(userSearch);
+
+  //});
+  //console.log(userSearch);
+//});
