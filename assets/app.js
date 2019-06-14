702f20cc80cdb49ea19ebb4bb8f1aab4ffc75180
Flippen, Ben <benflippen@gmail.com>
2019-06-13 19:07:40 -0500

    list change

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_4k2OJ7rzvz31qJ9fsiUWQtab8eqFJ8U",
+    authDomain: "regular-project-ffb1f.firebaseapp.com",
+    databaseURL: "https://regular-project-ffb1f.firebaseio.com",
+    projectId: "regular-project-ffb1f",
+    storageBucket: "regular-project-ffb1f.appspot.com",
+    messagingSenderId: "241952960961",
+    appId: "1:241952960961:web:bf48ab13524a7e2a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
