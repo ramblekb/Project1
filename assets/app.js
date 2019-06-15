24320acbab06814e6b2b0665ac27779c97a0cee6
Kip Bradley Smith <ramblekb@gmail.com>
2019-06-15 10:17:35 -0500

    Kip's stuff

--- assets/app.js
+++ assets/app.js
@@ -1,97 +1,50 @@
 
-
-
 $(document).ready(function () {
-     var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=band%7Cmusician&type=video&key=AIzaSyC4BfcpF2c8HeJU5ddlStqK6QeZHtdh31Q";
-       
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?&part=snippet&maxResults=5&q=band%7Cmusician&type=video&key=AIzaSyC4BfcpF2c8HeJU5ddlStqK6QeZHtdh31Q";
+      
 $.ajax({
-    url: queryURL,
-    method: "GET"
+   url: queryURL,
+   method: "GET"
 }).then(function(response) {
-    console.log(response);
+   console.log(response);
 });
-    // Variable that stores the favorite artist:
-    var favoritelist = [];
+   // Variable that stores the favorite artist:
+   var favoritelist = [];
 
 
-    // Function called when the form is submitted.
-    // Function adds favorite artist to the global array.
-    function addfavartist() {
+   // Function called when the form is submitted.
+   // Function adds favorite artist to the global array.
+   function addfavartist() {
 
-        var task = document.getElementById("nameInput");
-        var output = document.getElementById("userSearch");
-        var message = "";
+       var task = document.getElementById("nameInput");
+       var output = document.getElementById("userSearch");
+       var message = "";
 
-        if (task.value) {
-            favoritelist.push(task.value);
-            message = "";
-            for (var i = 0, count = favoritelist.length; i < count; i++) {
-                message += '<li>' + favoritelist[i] + '</li>';
-            }
+       if (task.value) {
+           favoritelist.push(task.value);
+           message = "";
+           for (var i = 0, count = favoritelist.length; i < count; i++) {
+               message += '<li>' + favoritelist[i] + '</li>';
+           }
 
-            output.innerHTML = message;
-        } // End of task.value IF.
+           output.innerHTML = message;
+       } // End of task.value IF.
 
-        // Return false to prevent submission:
-        return false;
+       // Return false to prevent submission:
+       return false;
 
-    }
+   }
 
 
-    function removeTask() {
+   function removeTask() {
 
-        alert("Button Clicked!");
+       alert("Button Clicked!");
 
-    }
+   }
 
-    $("#task").on("click", function (event) {
-        event.preventDefault();
-        addfavartist();
-    })
+   $("#task").on("click", function (event) {
+       event.preventDefault();
+       addfavartist();
+   })
 }
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById("nameInput").onclick  = function() {
-
-    //var favoritelist = document.createElement("Li");
-    //var text = document.getElementById("userSearch").value; 
-    //var textnode=document.createTextNode(text);
-    //node.appendChild(textnode);
-    //document.getElementById("userSearch").appendChild(node);
-
-
-
-
-//var userSearch = "";
-//$(document).ready(function() {
-//$("#buttonA").click(function() {
-    //userSearch = document.getElementById("userSearch").value;
-    //console.log(userSearch);
-
-  //});
-  //console.log(userSearch);
-//});
->>>>>>> origin/master
