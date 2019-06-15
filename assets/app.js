8d66c2ea1add4499f5de47c430feea6bb651eee8
Flippen, Ben <benflippen@gmail.com>
2019-06-15 10:23:27 -0500

    Fixed the bad merge

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyD_4k2OJ7rzvz31qJ9fsiUWQtab8eqFJ8U",
-    authDomain: "regular-project-ffb1f.firebaseapp.com",
-    databaseURL: "https://regular-project-ffb1f.firebaseio.com",
-    projectId: "regular-project-ffb1f",
-    storageBucket: "regular-project-ffb1f.appspot.com",
-    messagingSenderId: "241952960961",
-    appId: "1:241952960961:web:bf48ab13524a7e2a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-=======
-
-
 $(document).ready(function () {
     // Variable that stores the favorite artist:
     var favoritelist = [];
@@ -54,6 +38,18 @@ $(document).ready(function () {
     })
 }
 )
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_4k2OJ7rzvz31qJ9fsiUWQtab8eqFJ8U",
+    authDomain: "regular-project-ffb1f.firebaseapp.com",
+    databaseURL: "https://regular-project-ffb1f.firebaseio.com",
+    projectId: "regular-project-ffb1f",
+    storageBucket: "regular-project-ffb1f.appspot.com",
+    messagingSenderId: "241952960961",
+    appId: "1:241952960961:web:bf48ab13524a7e2a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 $(function () {
     $('li').css('cursor', 'pointer')
 
@@ -106,4 +102,3 @@ $("#form_group").reset();
   //});
   //console.log(userSearch);
 //});
->>>>>>> 8db66b9211fc5f7971bb819ab9587d7cb619928c
